027aa11e795dc12857755a1189494287dc09fd0d
ElvisKiongo <elvis.kiongo@student.moringaschool.com>
2024-06-11 20:51:19 +0300

    Added CRUD operations to orders.sql

--- orders.sql
+++ orders.sql
@@ -14,11 +14,18 @@ CREATE TABLE OrderItems (
     FOREIGN KEY (menu_id) REFERENCES Menu(menu_id)
 );
 
-INSERT INTO Orders (customer_id, order_date) VALUES
-(1, '2023-06-11 12:00:00'),
-(2, '2023-06-12 13:00:00');
+INSERT INTO Orders (customer_id, order_date) VALUES (3, '2023-06-13 14:00:00');
 
-INSERT INTO OrderItems (order_id, menu_id, quantity) VALUES
-(1, 1, 2),
-(1, 2, 1),
-(2, 1, 1);
+SELECT * FROM Orders;
+
+UPDATE Orders SET order_date = '2023-06-14 15:00:00' WHERE order_id = 1;
+
+DELETE FROM Orders WHERE order_id = 2;
+
+INSERT INTO OrderItems (order_id, menu_id, quantity) VALUES (1, 3, 2);
+
+SELECT * FROM OrderItems WHERE order_id = 1;
+
+UPDATE OrderItems SET quantity = 3 WHERE order_item_id = 1;
+
+DELETE FROM OrderItems WHERE order_item_id = 2;
